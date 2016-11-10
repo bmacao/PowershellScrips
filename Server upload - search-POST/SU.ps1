265b6cb534ca4ca16256220af8985633c4ce7934
bmacao <bruno_macao@hotmail.com>
2016-11-10 22:26:58 +0000

    Server upload script basead on user extension file search over local disk and upload to remote server using POST.
    The script uses 4 threads on upload action

---
+++ Server upload - search-POST/SU.ps1
@@ -0,0 +1,39 @@
+﻿#Get pre-requisites from user
+#Request file extension and Destination URL for upload
+$listdr = @(get-psdrive –psprovider filesystem | Select-Object -ExpandProperty Root)
+
+write-output "Discovered drives: "$listdr
+
+$Flext = Read-Host -Prompt 'Enter file extension for search'
+
+$DestURL = Read-Host -Prompt 'Enter destination URL'
+
+
+#Run search and parallel upload - 4 jobs simultaneously 
+workflow Test-Workflow{
+
+Param (
+        $driveslist,
+        $filext,
+        $URL
+         )
+
+
+foreach($drive in $driveslist){
+
+   $filesfound = Get-ChildItem -Path $driveslist -Filter "*.$filext" -Recurse  -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
+   $flscount = $filesfound.Count
+   $out1 = "Sending files - count: " + $flscount + " on drive " + $drive
+   Write-Output $out1
+   Foreach -Parallel -throttlelimit 4($file in $filesfound){ 
+    
+    $upload= Invoke-RestMethod -Uri $URL -Method Post -InFile $file -ContentType 'multipart/form-data' 
+  
+
+}
+
+}
+}
+
+
+Test-Workflow $listdr $Flext $DestURL
